5b6bb4d925d794079e06411a18003adb08c79261
Jacob (Jay) Jerrell <86279411+jjerrell@users.noreply.github.com>
2022-01-29 22:10:28 -0600

    Add things I did many moons ago and need to review

--- shared/src/commonMain/kotlin/app/jjerrell/lib/pocket/Platform.kt
+++ shared/src/commonMain/kotlin/app/jjerrell/lib/pocket/Platform.kt
@@ -3,6 +3,10 @@ package app.jjerrell.lib.pocket
 import io.ktor.client.*
 import kotlinx.coroutines.CoroutineDispatcher
 
+/**
+ * Basic information and a number of utilities that require (or benefit from) having platform
+ * specific implementations.
+ */
 expect class Platform() {
     val platform: String
     val applicationDispatcher: CoroutineDispatcher
--- shared/src/commonMain/kotlin/app/jjerrell/lib/pocket/service/Dto.kt
+++ shared/src/commonMain/kotlin/app/jjerrell/lib/pocket/service/Dto.kt
@@ -1,5 +1,9 @@
 package app.jjerrell.lib.pocket.service
 
+/**
+ * Establishes requirements for a standard data transfer object.
+ * Date/time properties are expected to be a measure of milliseconds since the epoch date.
+ */
 interface Dto {
     val id: String
     val name: String
